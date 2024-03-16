0e3f5b628d9c872d245c0d3955101c44fb4fd7d6
Vadim Smirnov <ukint-vs@proton.me>
2024-03-16 15:58:14 +0700

    fix(`sp-io`): Proper alloc panc handling (#31)

--- substrate/primitives/io/src/lib.rs
+++ substrate/primitives/io/src/lib.rs
@@ -1746,13 +1746,29 @@ mod allocator_impl {
 #[panic_handler]
 #[no_mangle]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
-	let message = sp_std::alloc::format!("{}", info);
-	#[cfg(feature = "improved_panic_error_reporting")]
-	{
+	use sp_std::alloc::{alloc::AllocErrorPanicPayload, format, string::String};
+
+	let improved_panic_error_reporting = match () {
+		#[cfg(feature = "improved_panic_error_reporting")]
+		() => true,
+		#[cfg(not(feature = "improved_panic_error_reporting"))]
+		() => false,
+	};
+
+	let message = info
+		.payload()
+		.downcast_ref::<AllocErrorPanicPayload>()
+		.map(|_| {
+			let msg = improved_panic_error_reporting
+				.then_some("Runtime memory exhausted.")
+				.unwrap_or("Runtime memory exhausted. Aborting");
+			String::from(msg)
+		})
+		.unwrap_or_else(|| format!("{info}"));
+
+	if improved_panic_error_reporting {
 		panic_handler::abort_on_panic(&message);
-	}
-	#[cfg(not(feature = "improved_panic_error_reporting"))]
-	{
+	} else {
 		logging::log(LogLevel::Error, "runtime", message.as_bytes());
 		core::arch::wasm32::unreachable();
 	}
